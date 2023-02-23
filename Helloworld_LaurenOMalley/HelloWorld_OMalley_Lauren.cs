d6cf77d2fb0edbfb7e1ad2bb9fc8fd72a9400009
lauren36 <111887636+lauren36@users.noreply.github.com>
2023-02-23 08:28:41 -0600

    Initial commit

---
+++ Helloworld_LaurenOMalley/HelloWorld_OMalley_Lauren.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Helloworld_LaurenOMalley
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {   // Asks the user for their name.
+            Console.WriteLine("Hello new user! Please type your first and last name below.";
+
+            // String that the user can use for their first name and last name.
+            string input = Console.ReadLine();
+
+            // Thanks the user and welcomes them to the program.
+            Console.WriteLine("Thank you {input}! Welcome to our wonderful program.");
+            
+        }
+    }
+}
---
+++ Helloworld_LaurenOMalley/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Helloworld_LaurenOMalley
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {// Asks the user for their name.
+            Console.WriteLine("Hello new user! Please type your first and last name below.";
+
+            // The place that the user can type their first name.
+            Console.ReadLine(firstName) 
+
+            // The place that the user can type their last name.
+            Console.ReadLine(lastName)
+
+            
+        }
+    }
+}
